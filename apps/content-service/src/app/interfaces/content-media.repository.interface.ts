db94a3cf53215d0283bf6cfcc0916ef5a9f172ad
farrelfauzan <farrelfauzan78@gmail.com>
2026-03-13 23:00:22 +0700

    feat: add profile picture

--- apps/content-service/src/app/interfaces/content-media.repository.interface.ts
+++ apps/content-service/src/app/interfaces/content-media.repository.interface.ts
@@ -1,8 +1,6 @@
 /// <reference types="multer" />
 
 export interface IContentMediaRepository {
-  uploadToGCS(file: Express.Multer.File, destination: string): Promise<string>;
   convertToHLS(inputPath: string, outputPath: string): Promise<void>;
-  deleteFromGCS(filePath: string): Promise<void>;
   getSignedUrl(filePath: string, expirationTime?: number): Promise<string>;
 }
---
+++ libs/src/prisma/migrations/20260312023632_add_profile_picture_url/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "admins" ADD COLUMN     "profile_picture_url" VARCHAR(500);
+
+-- AlterTable
+ALTER TABLE "customers" ADD COLUMN     "profile_picture_url" VARCHAR(500);
